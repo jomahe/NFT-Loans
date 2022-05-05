fac421c3ae6aef5d9ba9b310e2781bdd39b2ba52
regitche <tigerche207@gmail.com>
2022-05-04 20:12:48 -0700

    aligned form elements

--- frontend/components/CreateLoanForm.vue
+++ frontend/components/CreateLoanForm.vue
@@ -19,13 +19,13 @@
     </div>
     <div class="flex items-center">
       <span class="flex-1 inline-block">Minimum Duration</span>
-      <input type="text" placeholder="0" class="input flex-1">
-      <span class="flex-1 inline-block align-text-bottom">days</span>
+      <input type="text" placeholder="0" class="input flex-1" style = "position:relative; left:87px;">
+      <span class="flex-1 inline-block align-text-bottom">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;days</span>
     </div>
     <div class="flex items-center">
       <span class="flex-1 inline-block">Maximum Interest Rate</span>
-      <input type="text" placeholder="0" class="input flex-1">
-      <span class="flex-1 inline-block">%</span>
+      <input type="text" placeholder="0" class="input flex-1" style = "position:relative; left:87px;">
+      <span class="flex-1 inline-block">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;%</span>
     </div>
   </div>
 </template>
