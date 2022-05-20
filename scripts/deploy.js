d54b9ce1572ccca57dc787b0f813fe4ee8d2af8d
jomahe <100456673+jomahe@users.noreply.github.com>
2022-05-19 23:08:18 -0400

    verified contract, added to vue files

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,9 +11,14 @@ async function main() {
   const SmallLoanPool = await hre.ethers.getContractFactory("SmallLoan");
   const smallLoanPool = await SmallLoanPool.deploy(10);
 
+  const Collateral = await hre.ethers.getContractFactory("Collateral");
+  const collateral = await Collateral.deploy();
+
   await smallLoanPool.deployed();
+  await collateral.deployed();
 
   console.log("SmallLoanPool deployed to:", smallLoanPool.address);
+  console.log("Collateral deployed to:", collateral.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
