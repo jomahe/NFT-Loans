f518cabe8c25a234e4679b7dcec68042312e8a66
regitche <tigerche207@gmail.com>
2022-05-25 15:48:11 -0700

    asd

--- frontend/components/InstructionsBar.vue
+++ frontend/components/InstructionsBar.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="flex flex-row space-x-10 p-10 font-['Courier_New']">
-    <div class="flex-1 mx-auto">
-      <p>Borowers: </p>
-      <p>Put up an NFT as collateral to borrow tokens! </p>
-      <p>Hit "Create Loan", set loan terms and create your loan!</p>
+<div class="card bg-orange-300 rounded-none">
+  <div class="flex flex-row shadow-xl border-black font-['Courier_New']">
+    <div class="flex-1 ml-72 mt-10 mb-10">
+      <p class="font-bold">Borrowers: </p>
+      <p class="">Put up an NFT as collateral to borrow tokens! </p>
+      <p class="">Hit "Create Loan", set loan terms and create your loan!</p>
     </div>
-    <div class="flex-1 mx-auto">
-      <p>Lenders:</p>
+    <div class="flex-1 mr-72 mt-10 mb-10">
+      <p class="font-bold">Lenders:</p>
       <p>Earn interest from collateral-backed loans!</p>
       <p>Scroll through proposed loans and find ones that interest you!</p>
     </div>
-  </div>
+</div>
+</div>
 </template>
+
